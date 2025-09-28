25cc68b2fe16e07410b82f870238e705f75f7aa9
uta7770502 <yutakiyama@gmail.com>
2025-09-28 14:48:05 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,18 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('golf-rules-cache').then((cache) => cache.addAll([
-      '/',
-      '/index.html',
-      '/manifest.json',
-      '/icon-192.png',
-      '/icon-512.png'
-    ]))
+const CACHE_NAME = "golf-rule-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
