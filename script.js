70feebbe8d6c2f6c140f3933410fca2cbb2893b2
uta7770502 <yutakiyama@gmail.com>
2025-09-29 18:35:28 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const searchBox = document.getElementById("searchBox");
-  if (searchBox) {
-    searchBox.addEventListener("input", (e) => {
-      console.log("検索候補:", e.target.value);
-    });
-  }
-});
